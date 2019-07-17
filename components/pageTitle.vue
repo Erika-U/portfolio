6d25cd7ffc3744028832477d3decfa74d1cb5148
Erika-U <flower.errr@gmail.com>
2019-07-17 21:23:59 +0900

    create pageTitle component

---
+++ components/pageTitle.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2 class="title">
+    {{ title }}
+  </h2>
+</template>
+
+<script>
+  export default {
+    name: 'profile',
+    props: {
+      title: {
+        type: String,
+        default: ''
+      }
+    }
+  }
+</script>
