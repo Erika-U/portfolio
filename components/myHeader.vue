33c0ab10a5055883f4600f963504a7a62a27f954
Erika-U <flower.errr@gmail.com>
2019-07-21 22:04:16 +0900

    description of header

---
+++ components/myHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <div class="link">
+      <nuxt-link v-scroll-to="'#profile'" to>
+        Profile
+      </nuxt-link>
+      <nuxt-link v-scroll-to="'#skills'" to>
+        Skills
+      </nuxt-link>
+      <nuxt-link v-scroll-to="'#works'" to>
+        Works
+      </nuxt-link>
+      <nuxt-link v-scroll-to="'#hobbies'" to>
+        Hobbies
+      </nuxt-link>
+      <nuxt-link v-scroll-to="'#other'" to>
+        Other
+      </nuxt-link>
+    </div>
+  </header>
+</template>
+<script>
+  export default {
+    name: 'MyHeader'
+  }
+</script>
