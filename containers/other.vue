7dc4125b4791dee9015d052440c10c2fc24b8b81
Erika-U <flower.errr@gmail.com>
2019-07-22 17:30:48 +0900

    create other contents

--- containers/other.vue
+++ containers/other.vue
@@ -2,15 +2,10 @@
   <section id="other" class="other-wrapper">
     <pageTitle title="other" />
     <div class="box-wrapper">
-      <table>
-        <tbody>
-          <tr v-for="(contact, index) in contacts" :key="index">
-            <th>{{ contact.key }}</th>
-            <td>:</td>
-            <td>{{ contact.value }}</td>
-          </tr>
-        </tbody>
-      </table>
+      <div v-for="(contact, index) in contacts" :key="index">
+        <p class="other_title">{{ contact.key }}</p>
+        <p class="other_data">{{ contact.value }}</p>
+      </div>
     </div>
   </section>
 </template>
@@ -26,8 +21,12 @@
     data() {
       return {
         contacts: [
-          {key: 'Name',
-          value: 'erika'}
+          {key: '- 注目している技術 -',
+          value: 'アルゴレイヴ 、Webアニメーション'},
+          {key: '- webに興味を持ったきっかけ -',
+          value: 'デザインの流行があり、技術の進化のスピードが早く、ユーザーの使いやすさが求められるなど、他にも多くの要素で構成されていて奥が深いと思ったから。'},
+          {key: '- 自覚している性格 -',
+          value: 'とにかく真面目。行動力と根性には自信あり。好奇心旺盛で新しいことが好き。少し飽き性...。人と関わることが好きで、情に厚い。前向きで何事も最後まで諦めない粘り強さがある。少し突っ走りやすい。'}
         ]
       }
     }
@@ -41,8 +40,20 @@
   .other-wrapper {
     @include section;
 
-    table {
-      margin: 0 auto;
+
+    p {
+    margin: 0 auto;
+    max-width: 80%;
     }
-  }
+
+    .other_title {
+      font-size: 20px;
+      font-weight: 600;
+      margin-bottom: 10px;
+    }
+    .other_data {
+      width: 400px;
+      margin-bottom: 60px;
+      }
+    }
 </style>
